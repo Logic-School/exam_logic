6d9e4c547e06a787b1e6c1356cffebb71d4abdfa
Rizwaan Salim <argenspin@gmail.com>
2023-07-19 12:30:00 +0530

    Added additional fields and a pie chart for displaying result percentage

--- models/exam_details.py
+++ models/exam_details.py
@@ -1,15 +1,18 @@
 from odoo import models,fields,api
+from bokeh.plotting import figure 
+from bokeh.embed import components 
+import math  
 
 class ExamDetails(models.Model):
     _name = 'exam.details'
     name = fields.Char(string="Exam Name",compute = "_compute_name", store=True)
     date = fields.Date(string="Exam Date")
     exam_type = fields.Selection([('quarterly', 'Quarterly Exam'), ('topic', 'Topic wise Exam')],string='Exam Type')
+    topic = fields.Char(string="Topic")
     quart_percent = fields.Selection([('25','25%'), ('50', '50%'), ('75', '75%'), ('100', '100%')], string='Quarterly Percent')
     batch = fields.Many2one('logic.base.batch',string="Batch")
     classroom = fields.Many2one('logic.base.class',string="Class Room")
-    # faculty = fields.Many2one()
-    # student_attended = fields.Integer(compute='',string='Attended Students')
+    # coordinator = fields.Many2one('res.users',string="Coordinator",)
     pass_percentage = fields.Integer(string="Pass Percentage", compute="_compute_pass_fail_percentage")
     fail_percentage = fields.Integer(string="Fail Percentage", compute="_compute_pass_fail_percentage")
     faculty = fields.Many2one('res.users',string="Faculty", domain=[('faculty_check','=',True)])
@@ -18,8 +21,8 @@ class ExamDetails(models.Model):
     pass_mark = fields.Integer(string="Pass Mark")
     total_marks = fields.Integer(string="Total Marks")
     present_students = fields.Integer(string="Attended Students",compute="_compute_total_attendance")        
-    scored_marks = fields.Integer(string="Scored Marks", compute="_compute_scored_marks")
-
+    average_marks = fields.Float(string="Average Marks", compute="_compute_average_marks")
+    bokeh_chart = fields.Text(string="Result Chart",compute = "_compute_bokeh_chart")
     def create_student_results(self):
         for record in self:
             if not record.student_results:
@@ -33,6 +36,50 @@ class ExamDetails(models.Model):
                         'exam_id': record.id,
                     })
                 record.student_results = self.env['logic.student.result'].search([('exam_id','=',record.id)])
+
+    
+    @api.depends('pass_percentage','fail_percentage')
+    def _compute_bokeh_chart(self):
+        for record in self:
+            # Design your bokeh figure:
+            graph1 = figure(title="Results",plot_width=250, plot_height=220)  
+            fields = ['Pass','Fail']
+            percentages = [record.pass_percentage,record.fail_percentage]
+                            # formula for converting percentage into radians  
+            radians1 = [math.radians((percent / 100) * 360) for percent in percentages]  
+                
+            # Generating the start angle values  
+            start_angle = [math.radians(0)]  
+            prev = start_angle[0]  
+            for k in radians1[:-1]:  
+                start_angle.append(k + prev)  
+                prev = k + prev  
+                
+            # generating the end angle values  
+            end_angle = start_angle[1:] + [math.radians(0)]  
+                
+            # initiate the center of the pie chart  
+            x = 0  
+            y = 0  
+                
+            # then, we will initiate the radius of the glyphs  
+            radius = 1  
+                
+            # now, generate the color of the wedges  
+            color1 = ["green","red"]  
+                
+            # now, we will plot the graph  
+            for k in range(len(fields)):  
+                graph1.wedge(x, y, radius,  
+                            start_angle = start_angle[k],  
+                            end_angle = end_angle[k],  
+                            color = color1[k],  
+                            legend_label = fields[k])  
+            # `p.html.data` contains both markup and the script of a chart.script, div = components(p)
+            graph1.legend.glyph_height = 10#some int
+            graph1.legend.glyph_width = 10#some int
+            script, div = components(graph1)
+            record.bokeh_chart = '%s%s' % (div, script)
 
     @api.depends('student_results')
     def _compute_pass_fail_percentage(self):
@@ -64,19 +111,27 @@ class ExamDetails(models.Model):
             record.present_students = total
 
     @api.depends('student_results')
-    def _compute_scored_marks(self):
+    def _compute_average_marks(self):
         for record in self:
             total=0
             for result in record.student_results:
                 total += result.marks
-            record.scored_marks = total
+            if record.present_students > 0:
+                record.average_marks = total/record.present_students
+            else:
+                record.average_marks=0
+            
     @api.depends('date', 'exam_type', 'batch')
     def _compute_name(self):    
         for record in self:        
             date = fields.Date.from_string(
-                record.date).strftime('%Y-%m-%d')
-            exam_type = dict(self._fields['exam_type']._description_selection(self.env))[self.exam_type]
-            record.name = str(record.batch.name) + " " + str(exam_type) + str(date)
+                record.date).strftime('%Y/%m/%d')
+            # exam_type = dict(self._fields['exam_type']._description_selection(self.env))[self.exam_type]
+            if record.exam_type=="quarterly":
+                exam_type="Q"+record.quart_percent
+            else:
+                exam_type="T"
+            record.name = str(record.batch.name) + "-" + str(exam_type) + "-" + str(date)
     
     # @api.onchange('student_results')
     # def _compute_students(self):    
--- models/student_result.py
+++ models/student_result.py
@@ -8,11 +8,6 @@ class StudentExamResult(models.Model):
     def _get_default_exam_id(self):
         active_id = self.env.context.get('active_id')
         return active_id
-    exam_id = fields.Many2one("exam.details", string="Exam", domain="[('company_id', '=', current_company_id)]")
+    exam_id = fields.Many2one("exam.details", string="Exam",)
     marks = fields.Integer(string="Marks")
     present = fields.Boolean(string="Attendance")
-
-    # @api.onchange("student_id")    
-    # def onchange_student_id(self):
-    #     if self.student_id:    
-    #         self.name = self.student_id.name
